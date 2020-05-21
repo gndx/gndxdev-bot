7c6614591d11491144222d31ab5987ccd555f3c1
Oscar Barajas Tavares <oscar@dragons.mx>
2020-05-21 01:40:56 -0500

    Update commands.js

--- src/config/commands.js
+++ src/config/commands.js
@@ -4,7 +4,7 @@ const commands = {
   blog: 'Te comparto mi blog: https://gndx.dev y mi canal de YouTube donde comparto mis conferencias: https://youtube.com/c/oscarbarajas : Quieres apoyar mi contenido educativo visita mi Patreon: https://www.patreon.com/gndx',
   social: 'Mis Redes sociales: \n Instagram: https://instagram.com/gndx \n Twitter: https://twitter.com/gndx \n GitHub: https://github.com/gndx',
   courses: '🎓📗 Mis cursos en Platzi: https://platzi.com/teachers/gndx/',
-  'twitch': 'Recuerda todos los domingos a las 6:00 P.M. Hablo de tecnología en vivo en este canal, ¡Sígueme!'
+  twitch: 'Recuerda todos los domingos a las 6:00 P.M. Hablo de tecnología en vivo en este canal, ¡Sígueme!'
 };
 
 module.exports = commands;
