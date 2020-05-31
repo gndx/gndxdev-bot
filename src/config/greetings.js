0cc52520b70548ef6c2b1587137326e3ad00872b
Geovanny <williamnavarrocanon@gmail.com>
2020-05-31 14:00:13 -0500

    Update greetings.js

--- src/config/greetings.js
+++ src/config/greetings.js
@@ -3,6 +3,7 @@ const copies = require('../data/copies.json');
 const greetings = {
   welcome: copies.greetings.welcome,
   hello: copies.greetings.hello,
+  bye: copies.greetings.bye,
   subs: copies.greetings.subs
 };
 
